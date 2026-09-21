4dd2c29e50c73d8eb90a2e4635fde023d08ecd94
JoaoBruck <124901816+JoaoBruck@users.noreply.github.com>
2026-09-21 15:25:16 -0300

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='myu-1740-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r;}).catch(()=>caches.match('./index.html'))));
+});
